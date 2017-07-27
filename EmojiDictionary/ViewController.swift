b67f2d9cc31de9129752ad570c716066e2729c53
Ian Yao <sn0wasp3ct@gmail.com>
2017-07-26 18:09:52 -0700

    Not really working at the moment

--- EmojiDictionary/ViewController.swift
+++ EmojiDictionary/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableviewsin2017lul: UITableView!
+    
+    var emojis = ["😎", "💩", "😀", "👠", "🐹", "🐬", "💒"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        tableviewsin2017lul.dataSource = self
+        tableviewsin2017lul.delegate = self
 
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "LovelyChurchSegue", sender: emoji)
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any) {
+        print(sender)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
